b75da7355dd3864161723ead8f807a570220ea29
Hamdi <hamdi.bntiba@gmail.com>
2024-10-02 10:46:28 +0100

    Initial Commit

---
+++ TrainingBasketBall/TrainingBasketBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingBasketBallApp.swift
+//  TrainingBasketBall
+//
+//  Created by hamdi on 2/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingBasketBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
